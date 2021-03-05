51218d3cf492f925a04bcbe804aa26a7858921a8
alyaptsev <a.lyaptsev@elama.ru>
2021-03-05 12:06:50 +0500

    feat: main page w/o search panel

--- src/Pages/SearchScreen/SearchScreen.styled.ts
+++ src/Pages/SearchScreen/SearchScreen.styled.ts
@@ -1,5 +1,41 @@
 import styled from '@emotion/styled';
 
-export const HelloScreen = styled.h1`
-  color: red;
+export const StyledSeacrhScreen = styled.div`
+  position: relative;
+`;
+
+export const Background = styled.img`
+  width: 100%;
+`;
+
+export const ContentWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+`;
+
+export const LogoLink = styled.a`
+  display: inline-block;
+  margin-left: 16px;
+  margin-top: 16px;
+`;
+
+export const Tagline = styled.h2`
+  font-size: 26px;
+  font-weight: 400;
+  line-height: 36px;
+  margin: 86px 0 0 16px;
+  max-width: 223px;
+`;
+
+export const Partners = styled.div`
+  display: flex;
+  align-items: baseline;
+  margin-left: 16px;
+  margin-top: 33px;
+`;
+
+export const PartnerLogo = styled.img`
+  margin-right: 17px;
 `;
--- src/Pages/SearchScreen/SearchScreen.tsx
+++ src/Pages/SearchScreen/SearchScreen.tsx
@@ -1,6 +1,39 @@
 import React from 'react';
-import { HelloScreen } from './SearchScreen.styled';
+import {
+  StyledSeacrhScreen,
+  Background,
+  ContentWrapper,
+  LogoLink,
+  Tagline,
+  Partners,
+  PartnerLogo,
+} from './SearchScreen.styled';
+import backgroundImg from './assets/background.svg';
+import logoImg from './assets/logo.svg';
+import expediaImg from './assets/logo-expedia.svg';
+import bookingImg from './assets/logo-booking.svg';
+import hotelsImg from './assets/logo-hotels.svg';
+import agodaImg from './assets/logo-agoda.svg';
 
-const SearchScreen: React.FC = () => <HelloScreen>Search screen</HelloScreen>;
+const SearchScreen: React.FC = () => (
+  <StyledSeacrhScreen>
+    <Background src={backgroundImg} />
+
+    <ContentWrapper>
+      <LogoLink href="/">
+        <img alt="logo" src={logoImg} />
+      </LogoLink>
+
+      <Tagline>Find the perfect deal, always.</Tagline>
+
+      <Partners>
+        <PartnerLogo alt="partner logo: expedia" src={expediaImg} />
+        <PartnerLogo alt="partner logo: booking.com" src={bookingImg} />
+        <PartnerLogo alt="partner logo: hotels.com" src={hotelsImg} />
+        <PartnerLogo alt="partner logo: agoda" src={agodaImg} />
+      </Partners>
+    </ContentWrapper>
+  </StyledSeacrhScreen>
+);
 
 export default SearchScreen;
